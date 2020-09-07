b7ff342897d9abcbef3ad2cd879090b593ef2faa
Tom Zeller <tom-kouran@gmx.de>
2020-09-07 15:28:37 +0200

    change dialog

--- __init__.py
+++ __init__.py
@@ -20,10 +20,13 @@
 from aqt import mw, gui_hooks
 from aqt.qt import *
 import aqt
-from aqt.utils import showInfo, tooltip
+from aqt.utils import tooltip
 import os
 import re
 import typing
+import base64
+import time
+from typing import Any, Optional, Tuple
 
 def init_addon():
     """ Add action to "Tools" menu and register hook to check for _procgen.js file. """
@@ -33,6 +36,8 @@ def init_addon():
     mw.form.menuTools.addAction(tools_item)
 
     gui_hooks.collection_did_load.append(after_collection_did_load)
+    gui_hooks.webview_did_receive_js_message.append(expanded_on_bridge_cmd)
+
 
 
 def after_collection_did_load(col: "Collection"):
@@ -46,6 +51,24 @@ def open_main_dialog():
 
     InfoDialog(mw)
 
+def expanded_on_bridge_cmd(handled: Tuple[bool, Any], cmd: str, self: Any) -> Tuple[bool, Any]:
+
+    if cmd.startswith("procgen-save "):
+        b64 = cmd[13:]
+        if b64.startswith("data:image/png;base64,"):
+            b64 = b64[len("data:image/png;base64,"):]
+        if b64:
+            name = f"{files_save_folder()}{int(time.time())}.png"
+            if not os.path.isdir(files_save_folder()):
+                os.mkdir(files_save_folder()) 
+            with open(name, "wb") as fh:
+                fh.write(base64.b64decode(b64))
+            tooltip(f"Saved.")
+        return (True, None)
+
+    return handled
+
+
 def addon_folder() -> str:
     """ Absolute path to add-on folder. """
 
@@ -54,110 +77,65 @@ def addon_folder() -> str:
         return dir + "/"
     return dir
 
-class InfoDialog(QDialog):
-    """ Can be opened in the Tools menu, displays some help. """
+def files_save_folder() -> str:
+    """ Absolute path to add-on user_files folder. """
+    return addon_folder() + "user_files/"
+
+
+class SettingsTab(QWidget):
 
     def __init__(self, parent):
-        QDialog.__init__(self, parent, Qt.WindowSystemMenuHint | Qt.WindowTitleHint | Qt.WindowCloseButtonHint | Qt.WindowMaximizeButtonHint)
-
-        self.mw         = aqt.mw
-        self.parent     = parent
-
+        super(QWidget, self).__init__(parent)
+        self.config = mw.addonManager.getConfig(__name__) 
         self.setup_ui()
-        self.setWindowTitle("Random Sprites - Info")
+
 
     def setup_ui(self):
         layout = QVBoxLayout()
-
-        self.use_animation_cb = QCheckBox("Use Animation")
-        self.use_animation_cb.setChecked(True)
+        self.use_animation_cb = QCheckBox("Use Render Animation")
+        self.use_animation_cb.setChecked(self.config["use_render_animation"])
+        self.use_animation_cb.stateChanged.connect(self.update_render_animation)
+        layout.addWidget(QLabel("Should there be a short animation when the image is rendered?"))
         layout.addWidget(self.use_animation_cb)
+        layout.addSpacing(20)
 
-        self.scale_inp = QSpinBox()
-        self.scale_inp.setRange(1, 50)
-        self.scale_inp.setSingleStep(1)
-        self.scale_inp.setSuffix(" Pixel")
-        self.scale_inp.setValue(10)
-        sc_hb = QHBoxLayout()
-        sc_hb.addWidget(QLabel("Scale (Default 10)"))
-        sc_hb.addStretch(1)
-        sc_hb.addWidget(self.scale_inp)
-        layout.addLayout(sc_hb)
+        self.effects_cb = QCheckBox("Random animated effects")
+        self.effects_cb.setChecked(self.config["use_effect_animation"])
+        self.effects_cb.stateChanged.connect(self.update_effect_animation)
+        layout.addWidget(QLabel("Should there be a (random) animation effect after the image is rendered?"))
+        layout.addWidget(self.effects_cb)
+        layout.addSpacing(20)
 
         hbox = QHBoxLayout()
         replace_btn = QPushButton("Replace Script")
         replace_btn.clicked.connect(self.replace_js_file)
         hbox.addStretch(1)
         hbox.addWidget(replace_btn)
-        layout.addLayout(hbox)
-
+        hbox.addStretch(1)
+        layout.addStretch(1)
         line = QFrame()
         line.setFrameShape(QFrame.HLine)
         line.setFrameShadow(QFrame.Sunken) 
         layout.addWidget(line)
-
-        s_lbl = QLabel("<b>How to setup:</b>")
-        s_lbl.setAlignment(Qt.AlignCenter)
-        layout.addWidget(s_lbl)
-
-        layout.addWidget(QLabel("""
-Paste the following snippet into the card type of your choice.
-I recommend only inserting it on the backside, so that the image is shown
-after you answered a card.
-        """))
-
-        layout.addWidget(QLabel("1. Insert this where you want the image to appear:"))
-        le_1 = QLineEdit()
-        le_1.setText("<div id='procgen_canvas'></div>")
-        le_1.setReadOnly(True)
-        layout.addWidget(le_1)
-
-        layout.addWidget(QLabel("2. This will execute the script and fill 'procgen_canvas' with the image:"))
-        te_1 = QTextEdit()
-        te_1.setPlainText("""<script>
-const procgen_js = document.createElement('script');
-procgen_js.src = '_procgen.js';
-document.body.appendChild(procgen_js);
-</script>
-        """)
-        te_1.setReadOnly(True)
-        te_1.setMaximumHeight(100)
-        layout.addWidget(te_1)
-        # te_1.setFixedHeight(te_1.document().size().toSize().height() + 5)
-
-        layout.addWidget(QLabel("E.g. an example back side would look like:"))
-        te_2 = QTextEdit()
-        te_2.setPlainText("""<div style='margin-bottom: 20px;' id='procgen_canvas'></div>
-
-{{Front}}
-<hr/>
-{{Back}} 
-
-<script>
-const procgen_js = document.createElement('script');
-procgen_js.src = '_procgen.js';
-document.body.appendChild(procgen_js);
-</script>""")
-        te_2.setReadOnly(True)
-        layout.addWidget(te_2)
-
-        close = QPushButton("Close")
-        close.clicked.connect(self.accept)
-        hbottom = QHBoxLayout()
-        hbottom.addStretch(1)
-        hbottom.addWidget(close)
-        layout.addLayout(hbottom)
-
+        layout.addSpacing(10)
+        layout.addWidget(QLabel("Replaces the script (_procgen.js) in Anki's media folder with the current settings"))
+        layout.addSpacing(10)
+        layout.addLayout(hbox)
         self.setLayout(layout)
 
-        self.show()
+    def update_render_animation(self, state):
+        self.config["use_render_animation"] = self.use_animation_cb.isChecked()
+        mw.addonManager.writeConfig(__name__, self.config)
 
-
+    def update_effect_animation(self, state):
+        self.config["use_effect_animation"] = self.effects_cb.isChecked()
+        mw.addonManager.writeConfig(__name__, self.config)
+    
     def replace_js_file(self):
         """ Replace the _procgen.js file in the media folder with the file contained in the add-on folder. """
 
         use_animation   = self.use_animation_cb.isChecked()
-        scale           = self.scale_inp.value()
+        use_effect      = self.effects_cb.isChecked()
 
         if mw.col.media.have("_procgen.js"):
             mw.col.media.trash_files(["_procgen.js"])
@@ -171,15 +149,122 @@ document.body.appendChild(procgen_js);
 
         with open(addon_folder() + "_procgen_orig.js") as infile, open(addon_folder() + "_procgen.js", 'w') as outfile:
             for line in infile:
-                line = re.sub("USE_ANIMATION = (false|true)", "USE_ANIMATION = " + str(use_animation).lower(), line)
-                line = re.sub("SCALE = \\d+", f"SCALE = {scale}", line)
+                line = re.sub("USE_RENDER_ANIMATION = ?(false|true)", "USE_RENDER_ANIMATION = " + str(use_animation).lower(), line)
+                line = re.sub("USE_EFFECT_ANIMATION = ?(false|true)", "USE_EFFECT_ANIMATION = " + str(use_effect).lower(), line)
                 outfile.write(line)
 
         mw.col.media.addFile(addon_folder() + "_procgen.js")
 
         os.remove(addon_folder() + "_procgen.js")
         os.rename(addon_folder() + "_procgen_orig.js", addon_folder() + "_procgen.js")
-        tooltip(f"Replaced file in media folder.<br>SCALE = {scale}, USE_ANIMATION = {use_animation}")
+        tooltip(f"Replaced file in media folder.")
+
+class InfoTab(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.setup_ui()
+
+    def setup_ui(self):
+
+        layout = QVBoxLayout()
+        s_lbl = QLabel("<b>How to setup:</b>")
+        s_lbl.setAlignment(Qt.AlignCenter)
+        layout.addWidget(s_lbl)
+
+        layout.addWidget(QLabel("""
+Paste the following snippet into the card type of your choice.
+I recommend only inserting it on the backside, so that the image is shown
+after you answered a card.
+        """))
+
+        lbl_1 = QLabel("1. Insert this where you want the image to appear:")
+        lbl_1.setStyleSheet("border: 3px solid #2496dc;")
+        layout.addWidget(lbl_1)
+        le_1 = QLineEdit()
+        le_1.setText("<div id='procgen_canvas'></div>")
+        le_1.setReadOnly(True)
+        layout.addWidget(le_1)
+        layout.addSpacing(15)
+
+        lbl_2 = QLabel("2. This will execute the script and fill 'procgen_canvas' with the image:")
+        lbl_2.setStyleSheet("border: 3px solid #2496dc;")
+        layout.addWidget(lbl_2)
+        te_1 = QTextEdit()
+        te_1.setPlainText("""<script>
+const procgen_js = document.createElement('script');
+procgen_js.src = '_procgen.js';
+document.body.appendChild(procgen_js);
+</script>
+        """)
+        te_1.setReadOnly(True)
+        te_1.setMaximumHeight(100)
+        layout.addWidget(te_1)
+        # te_1.setFixedHeight(te_1.document().size().toSize().height() + 5)
+        layout.addSpacing(15)
+
+        lbl_3 = QLabel("E.g. an example back side would look like:")
+        lbl_3.setStyleSheet("border: 3px solid #2496dc;")
+        layout.addWidget(lbl_3)
+        te_2 = QTextEdit()
+        te_2.setPlainText("""<div style='margin-bottom: 20px;' id='procgen_canvas'></div>
+
+{{Front}}
+<hr/>
+{{Back}} 
+
+<script>
+if (!document.getElementById('procgen_js')) {
+    const procgen_js = document.createElement('script');
+    procgen_js.src = '_procgen.js';
+    document.body.appendChild(procgen_js);
+    procgen_js.setAttribute('id','procgen_js');
+}
+</script>""")
+        te_2.setReadOnly(True)
+        layout.addWidget(te_2)
+        self.setLayout(layout)
+
+
+
+class InfoDialog(QDialog):
+    """ Can be opened in the Tools menu, displays some help. """
+
+    def __init__(self, parent):
+        QDialog.__init__(self, parent, Qt.WindowSystemMenuHint | Qt.WindowTitleHint | Qt.WindowCloseButtonHint | Qt.WindowMaximizeButtonHint)
+
+        self.mw         = aqt.mw
+        self.parent     = parent
+
+        self.setup_ui()
+        self.setWindowTitle("Random Sprites")
+
+    def setup_ui(self):
+
+        layout              = QVBoxLayout()
+        self.setLayout(layout)
+        self.tabs           = QTabWidget()
+        self.tab_settings   = SettingsTab(self) 
+        self.tab_info       = InfoTab(self) 
+
+        self.tabs.addTab(self.tab_settings, "Settings")
+        self.tabs.addTab(self.tab_info, "Info")
+        self.layout().addWidget(self.tabs)
+
+        close = QPushButton("Close")
+        close.clicked.connect(self.accept)
+        hbottom = QHBoxLayout()
+        hbottom.addStretch(1)
+        hbottom.addWidget(close)
+        layout.addLayout(hbottom)
+        self.show()
+
+
+    
+
+
+
+
 
 
 init_addon()
